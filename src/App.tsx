c6cde22e8a2cb2969fecf65069b73e6172ff9aea
Ryan Shee <7772995+LIONisaQT@users.noreply.github.com>
2025-05-06 16:35:51 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="playArea">
+      <div className="topArea">
+        <div className="foundations">foundation</div>
+        <div className="stockAndWaste">
+          <div className="waste">waste</div>
+          <div className="stock">stock</div>
+        </div>
+      </div>
+      <div className="tableau">Tableau</div>
+    </div>
+  );
+}
+
+export default App;
